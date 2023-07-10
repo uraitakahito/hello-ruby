1e06b9273f5d91219d51acb3391f7a9086d31449
Takahito Urai <uraitakahito@gmail.com>
2023-07-10 17:03:17 +0900

    Add sample

---
+++ hoge.rb
@@ -0,0 +1,5 @@
+# https://zenn.dev/jnchito/articles/24e0bd7fd1045d#%E3%81%9D%E3%81%AE%E4%BB%96%E3%81%AE%E6%B3%A8%E7%9B%AE%E3%83%9D%E3%82%A4%E3%83%B3%E3%83%88
+# 右代入
+data = {name: 'Alice', age: 20, zodiac: 'Capricorn'}
+data => {name:, zodiac:}
+p name   #=> "Alice"
