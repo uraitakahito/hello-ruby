14228722d1fc1ee443b2d056ace2b93ee2b8e9e1
Takahito Urai <uraitakahito@gmail.com>
2024-05-19 17:39:25 +0900

    Add Shopify.ruby-lsp.

--- .devcontainer/zshrc-entrypoint-init.d/install-vscode-ruby-extensions.sh
+++ .devcontainer/zshrc-entrypoint-init.d/install-vscode-ruby-extensions.sh
@@ -5,6 +5,8 @@
 extensions=(
   # https://marketplace.visualstudio.com/items?itemName=KoichiSasada.vscode-rdbg
   "KoichiSasada.vscode-rdbg"
+  # https://marketplace.visualstudio.com/items?itemName=Shopify.ruby-lsp
+  "Shopify.ruby-lsp"
 )
 for extension in ${extensions[@]}; do
   code --install-extension $extension
